a35aa36b001f866d915d732ab7add0369d12c1e4
Divaldo <antifraudeuda@gmail.com>
2021-09-16 17:33:19 -0600

    true

--- DevPractica3/settings.py
+++ DevPractica3/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '8lrty#un*%!1^vd+-vmt8ad_nxho7*a7c=0@k&r_b8=w+^h311'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
